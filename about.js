eaf0b26238c486a05fd33a2a9caf2593ab9f4a4a
Devansh422 <verma0007.dev@gmail.com>
2026-01-30 17:51:12 +0530

    added chanegs

--- about.js
+++ about.js
@@ -1,9 +1,8 @@
 (function(){
   const content = `
-      <h2>About Us</h2>
       <div class="about-container">
         <div style="width: 50%"><img src="assets/director.png" style="width:400px; height:auto;" class="about-image" alt="About Us" /></div>
-        <div class="about-details">Twin Tech India Pvt. Ltd is a premier manufacturer and supplier of aerosols, sprays, and specialty machines for various industries. Our flagship brand, SPRAYZET, offers custom-designed, biodegradable aerosols for applications in agriculture, engineering, textiles, food manufacturing, and more. We also provide contract aerosol packaging with private label capabilities, ensuring quality and efficiency.<br><br>Our diverse product line includes industrial maintenance aerosols, self-adhesive tapes, home care products under "Hacker," air deodorizers under "Twinzy," and car care solutions under "Evershine." We are committed to high standards, innovation, and timely delivery.</div>
+        <div class="about-details"><h1 style-="line-height : 1; padding-top: 0;">About Us</h1><br> Twin Tech India Pvt. Ltd is a premier manufacturer and supplier of aerosols, sprays, and specialty machines for various industries. Our flagship brand, SPRAYZET, offers custom-designed, biodegradable aerosols for applications in agriculture, engineering, textiles, food manufacturing, and more. We also provide contract aerosol packaging with private label capabilities, ensuring quality and efficiency.<br><br>Our diverse product line includes industrial maintenance aerosols, self-adhesive tapes, home care products under "Hacker," air deodorizers under "Twinzy," and car care solutions under "Evershine." We are committed to high standards, innovation, and timely delivery.</div>
       </div>
   `;
 
